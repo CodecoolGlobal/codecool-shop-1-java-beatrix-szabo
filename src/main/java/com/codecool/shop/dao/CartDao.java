b67b828a612fb08aaac4eb2f4883ab25e535bb7f
Ufo420 <pelyheroland22@gmail.com>
2021-08-04 14:05:21 +0200

    one step forward to make a cart working

---
+++ src/main/java/com/codecool/shop/dao/CartDao.java
@@ -0,0 +1,13 @@
+package com.codecool.shop.dao;
+
+import com.codecool.shop.model.Cart;
+import com.codecool.shop.model.Product;
+
+import java.util.ArrayList;
+
+public interface CartDao {
+    void add(Product product);
+    void remove(Product product);
+    ArrayList<Product> getAll();
+
+}
--- src/main/java/com/codecool/shop/model/Cart.java
+++ src/main/java/com/codecool/shop/model/Cart.java
@@ -4,9 +4,11 @@ import com.codecool.shop.model.Product;
 import java.util.ArrayList;
 
 public class Cart {
+
     public ArrayList<Product> getProductCartList() {
         return productCartList;
     }
+
 
     public void add(Product product){
         this.productCartList.add(product);
