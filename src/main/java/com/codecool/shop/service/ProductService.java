9165c18ac18a472aae6758edd6de67d19c0d0475
beatrix-szabo <beatrix.szabo.sc@gmail.com>
2021-08-04 15:53:42 +0200

    Fixed Option filters. Every options works fine

--- src/main/java/com/codecool/shop/service/ProductService.java
+++ src/main/java/com/codecool/shop/service/ProductService.java
@@ -2,6 +2,7 @@ package com.codecool.shop.service;
 
 import com.codecool.shop.dao.ProductCategoryDao;
 import com.codecool.shop.dao.ProductDao;
+import com.codecool.shop.dao.SupplierDao;
 import com.codecool.shop.model.Product;
 import com.codecool.shop.model.ProductCategory;
 
@@ -10,10 +11,12 @@ import java.util.List;
 public class ProductService{
     private ProductDao productDao;
     private ProductCategoryDao productCategoryDao;
+    private SupplierDao supplierDao;
 
-    public ProductService(ProductDao productDao, ProductCategoryDao productCategoryDao) {
+    public ProductService(ProductDao productDao, ProductCategoryDao productCategoryDao, SupplierDao supplierDao) {
         this.productDao = productDao;
         this.productCategoryDao = productCategoryDao;
+        this.supplierDao = supplierDao;
     }
 
     public ProductCategory getProductCategory(int categoryId){
@@ -25,4 +28,7 @@ public class ProductService{
         return productDao.getBy(category);
     }
 
+    public List<Product> getProductsForSupplier(int supplierId) {
+        return productDao.getBy(supplierDao.find(supplierId));
+    }
 }
