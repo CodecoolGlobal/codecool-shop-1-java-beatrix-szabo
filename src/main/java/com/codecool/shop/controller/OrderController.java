55ba425676c21699eb0f697bcc7fa2434f4b9ef7
KriszTuri <turi.krisztina1999@gmail.com>
2021-08-17 10:22:53 +0200

    order page added, so far nothing major, just a base

--- src/main/java/com/codecool/shop/controller/OrderController.java
+++ src/main/java/com/codecool/shop/controller/OrderController.java
@@ -1,12 +1,8 @@
 package com.codecool.shop.controller;
 
 import com.codecool.shop.config.TemplateEngineUtil;
-import com.codecool.shop.dao.CartDao;
-import com.codecool.shop.dao.implementation.CartDaoMem;
-import com.codecool.shop.model.Product;
 import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.context.WebContext;
-import java.util.HashMap;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -14,22 +10,12 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-
-@WebServlet(urlPatterns = {"/cart"})
-public class CartController extends HttpServlet {
-
-
-    @Override
+@WebServlet(urlPatterns = {"/order"})
+public class OrderController extends HttpServlet{
     protected void doGet(HttpServletRequest req, HttpServletResponse rep) throws ServletException, IOException{
-        CartDao cart = CartDaoMem.getInstance();
         TemplateEngine engine = TemplateEngineUtil.getTemplateEngine(req.getServletContext());
         WebContext context = new WebContext(req, rep, req.getServletContext());
-        context.setVariable("fullMoney", cart.CountProductByAll());
-        //context.setVariable("MoneyByEach", cart.CountProductsByEachPrice());
-
-        context.setVariable("cart", cart);
-        engine.process("product/Cart.html", context, rep.getWriter());
+        engine.process("product/Order.html", context, rep.getWriter());
     }
 
 }
-
