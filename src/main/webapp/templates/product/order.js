0416b923d04bef0fd0501fea80f56977c89a4120
Ufo420 <pelyheroland22@gmail.com>
2021-08-30 11:41:04 +0200

    paypal Works

--- src/main/webapp/templates/product/order.js
+++ src/main/webapp/templates/product/order.js
@@ -1,6 +1,14 @@
 document.getElementById("test").addEventListener("click", test);
-
+$("paymentcheck").addEventListener("click", paypal);
 function test(){
     console.log("oaeshrfopuewh");
+}
+
+function paypal(){
+    console.log("kicserélek mindent");
+    var paymentDiv = document.getElementById("payment");
+    paymentDiv.innerHTML = "";
+    paymentDiv.appendChild(<label for="paypalmail"> Paypal email</label>);
+    paymentDiv.appendChild(<input type="text" id="paypalmail" placeholder="example@email.com">);
 }
 
